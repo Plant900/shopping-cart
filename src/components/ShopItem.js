f87a0dbc92113a59fc42f3d8770f0f3c83d35976
Plant900 <rythodir@gmail.com>
2022-05-20 12:53:13 -0600

    Edit class names

--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
+import { NavLink } from 'react-router-dom'
 
 export const ShopItem = ({ shopItem, addToCart }) => {
   let { name, price, image, id } = shopItem
@@ -12,7 +13,9 @@ export const ShopItem = ({ shopItem, addToCart }) => {
 
   return (
     <div className={'shopItem'}>
-      <img src={image} />
+      <NavLink to={`product/${name}`}>
+        <img src={image} />
+      </NavLink>
       <div className="shop-item-name">{name}</div>
       <div className="shop-item-price">{price}</div>
       <div className="shop-item-btn-container">
