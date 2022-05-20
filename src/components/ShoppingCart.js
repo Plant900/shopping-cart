13ab1b9e55d1755734ab95c37ff681b4de329c3d
Plant900 <rythodir@gmail.com>
2022-05-20 12:53:47 -0600

    Correctly round price info and add delete button

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,5 +1,35 @@
 import React from 'react'
+import { useState } from 'react'
 
-export const ShoppingCart = () => {
-  return <div>Shopping Cart</div>
+export const ShoppingCart = ({ cartItems, changeItemQuantity, removeItem }) => {
+  let [value, setValue] = useState(0)
+
+  function handleChange(e) {
+    setValue(e.target.value)
+  }
+
+  return cartItems.map((cartItem) => {
+    return (
+      <div className="cart-item" id={cartItem.id}>
+        <img src={cartItem.image} />
+        <div className="cart-item-name">{cartItem.name}</div>
+        <div className="cart-item-price">
+          {`$
+          ${
+            Math.round(cartItem.pricedata * Number(cartItem.quantity) * 100) /
+            100
+          }`}
+        </div>
+        <input
+          type="number"
+          value={cartItem.quantity}
+          onChange={(e) => {
+            handleChange(e)
+            changeItemQuantity(cartItem.id, value)
+          }}
+        ></input>
+        <button onClick={() => removeItem(cartItem.id)}>Remove</button>
+      </div>
+    )
+  })
 }
