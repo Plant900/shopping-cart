be217fe70effedfcfd000a53251b293dcb04d829
Plant900 <rythodir@gmail.com>
2022-05-20 12:51:30 -0600

    Add ability to click on shop item for more detailed view

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css'
 import uniqid from 'uniqid'
-import { useState, useEffect } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { useState, useEffect, useDebugValue } from 'react'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import { Home } from './components/Home'
 import { ShoppingCart } from './components/ShoppingCart'
 import { NavBar } from './components/NavBar'
+import { ItemPage } from './components/ItemPage'
 
 function App() {
   let [cartAmount, setCartAmount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
     {
       name: '6oz Ceramic Mug',
       price: '$12.97',
+      pricedata: 12.97,
       image:
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTARtLrqe-rAdhh5naeXSe0XZQRHInAQJAo0g&usqp=CAU',
       id: uniqid(),
@@ -20,14 +22,24 @@ function App() {
     {
       name: '12oz Ceramic Mug',
       price: '$14.97',
+      pricedata: 14.97,
       image: 'https://i.ebayimg.com/images/g/B9oAAOSw9jdgvpOb/s-l400.jpg',
       id: uniqid(),
     },
     {
       name: 'Black Tea, 16 bags',
       price: '$15.97',
+      pricedata: 14.97,
       image:
         'https://s.cornershopapp.com/product-images/2417931.jpg?versionId=Z42g42dh16s63FGFvZBB7cpk.V7_Z1q1',
+      id: uniqid(),
+    },
+    {
+      name: '16oz Glass Mug with Infuser and Lid',
+      price: '$23.97',
+      pricedata: 23.97,
+      image:
+        'https://www.divinitea.com/wp-content/uploads/cm/g/l/glass_tall_tea_mug_1_700.jpg',
       id: uniqid(),
     },
   ])
@@ -41,13 +53,9 @@ function App() {
     if (cartItems.some((cartItem) => cartItem.id === id)) {
       let alreadyInCart = cartItems.find((cartItem) => cartItem.id === id)
       let index = cartItems.indexOf(alreadyInCart)
+      let quantity = amount + alreadyInCart.quantity
 
-      let updatedCartItems = [...cartItems]
-
-      updatedCartItems[index].quantity =
-        updatedCartItems[index].quantity + amount
-
-      setCartItems(updatedCartItems)
+      changeItemQuantity(id, quantity)
     } else {
       let shopItem = shopItems.find((shopItem) => {
         return shopItem.id === id
@@ -57,7 +65,33 @@ function App() {
     }
   }
 
+  function changeItemQuantity(id, amount) {
+    let itemInCart = cartItems.find((cartItem) => cartItem.id === id)
+    let index = cartItems.indexOf(itemInCart)
+
+    let updatedCartItems = [...cartItems]
+
+    updatedCartItems[index].quantity = Number(amount)
+
+    setCartItems(updatedCartItems)
+  }
+
+  function removeItem(id) {
+    let itemInCart = cartItems.find((cartItem) => cartItem.id === id)
+    let index = cartItems.indexOf(itemInCart)
+
+    let updatedCartItems = [...cartItems]
+
+    updatedCartItems.splice(index, 1)
+
+    setCartItems(updatedCartItems)
+  }
+
   useEffect(() => {
+    if (cartItems.length === 0) {
+      setCartAmount(0)
+    }
+
     if (cartItems.length > 0) {
       setCartAmount(
         Number(cartItems.reduce((sum, current) => sum + current.quantity, 0))
@@ -66,16 +100,29 @@ function App() {
   }, [cartItems])
 
   return (
-    <>
+    <BrowserRouter>
       <NavBar cartAmount={cartAmount} />
       <Routes>
         <Route
           path="/"
           element={<Home shopItems={shopItems} addToCart={addToCart} />}
         ></Route>
-        <Route path="cart" element={<ShoppingCart />}></Route>
+        <Route
+          path="cart"
+          element={
+            <ShoppingCart
+              cartItems={cartItems}
+              changeItemQuantity={changeItemQuantity}
+              removeItem={removeItem}
+            />
+          }
+        ></Route>
+        <Route
+          path="product/:Item"
+          element={<ItemPage shopItems={shopItems} />}
+        ></Route>
       </Routes>
-    </>
+    </BrowserRouter>
   )
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,127 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
+import App from './App'
+import { Home } from './components/Home'
+import { NavBar } from './components/NavBar'
+import { unmountComponentAtNode } from 'react-dom'
+import { act } from 'react-dom/test-utils'
+import { ShopItem } from './components/ShopItem'
+import { MemoryRouter } from 'react-router-dom'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+let container = null
+beforeEach(() => {
+  container = document.createElement('div')
+  document.body.appendChild(container)
+})
+
+afterEach(() => {
+  unmountComponentAtNode(container)
+  container.remove()
+  container = null
+})
+
+describe('App component', () => {
+  it('renders a navbar', () => {
+    const { container } = render(<App />)
+    expect(container).toMatchSnapshot()
+  })
+})
+
+describe('ShopItem component', () => {
+  it('renders when given props', () => {
+    const { getByRole, getByText } = render(
+      <MemoryRouter>
+        <ShopItem
+          shopItem={{
+            name: 'Mug',
+            price: '$10',
+            image: 'fakeurl',
+            id: '123',
+          }}
+        />
+      </MemoryRouter>
+    )
+
+    expect(getByRole('button').textContent).toMatch('Add to cart')
+    expect(getByText('Mug')).toBeInTheDocument()
+    expect(getByText('$10')).toBeInTheDocument()
+  })
+})
+
+describe('Home component', () => {
+  it('renders correct number of items', () => {
+    const { getByRole, getAllByRole } = render(
+      <MemoryRouter>
+        <Home
+          shopItems={[
+            {
+              name: 'Mug',
+              price: '$10',
+              image: 'fakeurl',
+              id: '123',
+            },
+            {
+              name: 'Mug2',
+              price: '$11',
+              image: 'fakeurl',
+              id: '1234',
+            },
+          ]}
+        />
+      </MemoryRouter>
+    )
+
+    const buttons = getAllByRole('button')
+    expect(buttons.length).toBe(2)
+  })
+
+  it('calls addToCart when a shop item add button is clicked', () => {
+    const addToCart = jest.fn()
+
+    const { getAllByRole } = render(
+      <MemoryRouter>
+        <Home
+          shopItems={[
+            {
+              name: 'Mug',
+              price: '$10',
+              image: 'fakeurl',
+              id: '123',
+            },
+            {
+              name: 'Mug2',
+              price: '$11',
+              image: 'fakeurl',
+              id: '1234',
+            },
+          ]}
+          addToCart={addToCart}
+        />
+      </MemoryRouter>
+    )
+
+    const inputs = getAllByRole('spinbutton')
+    const buttons = getAllByRole('button')
+
+    userEvent.type(inputs[0], '1')
+    userEvent.click(buttons[0])
+
+    expect(addToCart).toHaveBeenCalledTimes(1)
+  })
+})
+
+describe('NavBar component', () => {
+  it('should update Cart() with number', () => {
+    const { getByRole, getByText, get } = render(
+      <MemoryRouter>
+        <NavBar cartAmount={3} />
+      </MemoryRouter>
+    )
+    screen.debug()
+
+    const counter = getByText('(3)')
+
+    expect(counter).toBeInTheDocument()
+  })
+})
